3f87c5b3aae983ca81d4050756d5a4179c877559
aakash <goelaakash79@gmail.com>
2017-11-02 09:09:12 +0530

    resturant

--- app/src/main/java/com/example/aakash/restura_de_ordera/ItemsListActivity.java
+++ app/src/main/java/com/example/aakash/restura_de_ordera/ItemsListActivity.java
@@ -1,11 +1,10 @@
 package com.example.aakash.restura_de_ordera;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class ItemsListActivity extends AppCompatActivity {
 
@@ -14,7 +13,7 @@ public class ItemsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items_list);
 
-        ImageView dosa = (ImageView) findViewById(R.id.dosa);
+        Button dosa = (Button) findViewById(R.id.orderDosa);
         dosa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/aakash/restura_de_ordera/MainActivity.java
+++ app/src/main/java/com/example/aakash/restura_de_ordera/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView logo = (ImageView) findViewById(R.id.imageView);
+        logo.animate().alpha(1f).scaleX(0.9f).scaleY(0.9f).setDuration(2500);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, ItemsListActivity.class);
-                startActivity(intent);
+                Intent i = new Intent(getApplicationContext(), ItemsListActivity.class);
+                startActivity(i);
+                finish();
             }
-        }, 1000);
+        }, 3600);
 
     }
 }
